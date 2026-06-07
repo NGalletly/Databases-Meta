e5c20f7d6b754b7afc4ab015d1d4865ad71232ec
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-06-07 10:29:58 +0100

    string datatypes

--- Assignments/working_with_numbers/sql_statements.sql
+++ Assignments/working_with_numbers/sql_statements.sql
@@ -6,4 +6,10 @@ USE cm_devices;
 CREATE TABLE devices(device_id INT(50),name VARCHAR(60), price DECIMAL);
 CREATE TABLE stock(device_id INT, quantity INT, price DECIMAL);
 
-/* SHOW COLUMNS FROM devices;
+/* SHOW COLUMNS FROM devices; */
+
+
+/* STRINGS DATATYPES */
+
+CREATE TABLE customers(username VARCHAR (15), full_name VARCHAR (50), email VARCHAR(50));
+CREATE TABLE feedback(feedback_id INT, CATEGORY VARCHAR (20), comment TEXT(500));
