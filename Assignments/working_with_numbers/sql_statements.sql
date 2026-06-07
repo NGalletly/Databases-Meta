3b3203044539644d93197a5c79d5fec2fd561f8e
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-06-07 10:45:08 +0100

    mixed data types

--- Assignments/working_with_numbers/sql_statements.sql
+++ Assignments/working_with_numbers/sql_statements.sql
@@ -13,3 +13,13 @@ CREATE TABLE stock(device_id INT, quantity INT, price DECIMAL);
 
 CREATE TABLE customers(username VARCHAR (15), full_name VARCHAR (50), email VARCHAR(50));
 CREATE TABLE feedback(feedback_id INT, CATEGORY VARCHAR (20), comment TEXT(500));
+
+/* DEFAULT VALUES: */
+CREATE TABLE player(name VARCHAR(50) NOT NULL, city VARCHAR(30) DEFAULT 'Barcelona');
+
+CREATE TABLE address_table(customer_id INT NOT NULL, street VARCHAR(50) NOT NULL, postcode VARCHAR (10) DEFAULT "HA97DE", town VARCHAR(25) DEFAULT "Harrow");
+
+
+/* MIXED DATA TYPES */
+CREATE TABLE invoice(full_name VARCHAR(50) NOT NULL, order_date DATE, Quantity INT, price DECIMAL);
+CREATE TABLE customer_account(account_number INT NOT NULL, phone_number VARCHAR(14) NOT NULL, email VARCHAR(50) NOT NULL);
