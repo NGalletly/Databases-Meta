c476524c8102c4dc359a936bf36719401b4b933c
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-06-12 13:48:11 +0100

    Added foreign key

--- Assignments/car_table/car_table.sql
+++ Assignments/car_table/car_table.sql
@@ -3,5 +3,6 @@ USE automobile;
 CREATE TABLE vehicle(VehicleID varchar(15), OwnerID INT, CarPlateNumber VARCHAR(15), OwnerPhoneNumber VARCHAR(15), PRIMARY KEY (VehicleID));
 CREATE TABLE owner(OwnerID VARCHAR(15), OwnerName VARCHAR(20), OwnerAddress VARCHAR (50), PRIMARY KEY(OwnerID));
 
+ALTER TABLE vehicle ADD FOREIGN KEY (ownerID) REFERENCES owner(ownerId);
 
 
